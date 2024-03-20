9fe447a548d612c1ba874aa5a53ac6d7122e41ae
leandro camargo more <112519932+leandrocmore@users.noreply.github.com>
2024-03-20 17:00:03 -0300

    Update settings.py

--- ProjetoApp/settings.py
+++ ProjetoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG","False").lower() == "true" 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split('')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(",")
 
 
 # Application definition
@@ -129,4 +129,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
