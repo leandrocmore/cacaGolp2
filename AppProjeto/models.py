2b00497d6cdde87d59694adfc6c79c2a1d033640
leandro camargo more <112519932+leandrocmore@users.noreply.github.com>
2024-04-03 16:21:25 -0300

    teste

--- AppProjeto/models.py
+++ AppProjeto/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class UserModels(models.Model):
     id_Gps = models.AutoField(primary_key=True)
-    latitude = models.IntegerField()
-    longitude = models.IntegerField()
+    latitude = models.DecimalField(max_digits=2,decimal_places=9)
+    longitude = models.DecimalField(max_digits=2,decimal_places=9)
 
     def __str__(self):
         return f'id_Gps: {self.id_Gps} | latitude: {self.latitude} | longitude: {self.longitude}'
