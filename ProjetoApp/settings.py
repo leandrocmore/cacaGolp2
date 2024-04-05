2963934fa94928e180a269a1a988a309d2c79c44
leandro camargo more <112519932+leandrocmore@users.noreply.github.com>
2024-04-05 20:35:24 -0300

    teste

--- ProjetoApp/settings.py
+++ ProjetoApp/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG","False").lower() == "true" 
 
-ALLOWED_HOSTS = ['cacagolp2-5.onrender.com' ]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 #ALLOWED_HOSTS = ['cacagolp2-5.onrender.com' ]
 
